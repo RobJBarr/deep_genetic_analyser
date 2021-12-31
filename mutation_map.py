6c0ad173cab8c2e06bbfc640090cd4ccadcbaa8a
Prashant Gurung <prashant.grg07@gmail.com>
2021-12-31 22:54:17 +0000

    uses pickle file to load in model

--- mutation_map.py
+++ mutation_map.py
@@ -1,7 +1,12 @@
 import numpy as np
+import torch
+
+from util import read_file
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
-def generate_mutation_map(seq, model):
+def generate_mutation_map(seq):
     """
     Given a sequence s = (s1,..,sn) of length n and
     DeepBind model, generate (4 x n) matrix
@@ -9,12 +14,17 @@ def generate_mutation_map(seq, model):
 
     mutation_map = np.zeros(shape=(len(seq), 4))
     base = ['A', 'C', 'G', 'T']
+
+    model = read_file("part2_model.pickle").to(device)
     score = model.predict(seq)
+    print('Score:' + str(score))
 
     for i in range(mutation_map.shape[0]):
         for j in range(mutation_map.shape[1]):
-            mutated_seq = seq[:i] + base[j] + seq[i + i:]
+            mutated_seq = seq[:i] + base[j] + seq[i + 1:]
+            print('Mutated Sequence:' + mutated_seq)
             new_score = model.predict(mutated_seq)
+            print('New score:' + str(new_score))
             mutation_map[i, j] = (new_score - score) * max(0, score, new_score)
 
     return mutation_map.T
@@ -25,5 +35,3 @@ def get_mutation_map(map):
     Given a mutation map, generate an interaction logo
     """
     pass
-
-
