c7dc34f5e9314ea8876bf786916e818bce86f760
Prashant Gurung <prashant.grg07@gmail.com>
2022-01-01 17:43:49 +0000

    makes and saves mutation map, needs to work for long sequences

--- mutation_map.py
+++ mutation_map.py
@@ -1,3 +1,5 @@
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -6,7 +8,7 @@ from util import read_file
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
-def generate_mutation_map(seq):
+def get_mutation_map(seq, model):
     """
     Given a sequence s = (s1,..,sn) of length n and
     DeepBind model, generate (4 x n) matrix
@@ -15,23 +17,153 @@ def generate_mutation_map(seq):
     mutation_map = np.zeros(shape=(len(seq), 4))
     base = ['A', 'C', 'G', 'T']
 
-    model = read_file("part2_model.pickle").to(device)
     score = model.predict(seq)
-    print('Score:' + str(score))
-
     for i in range(mutation_map.shape[0]):
         for j in range(mutation_map.shape[1]):
             mutated_seq = seq[:i] + base[j] + seq[i + 1:]
-            print('Mutated Sequence:' + mutated_seq)
             new_score = model.predict(mutated_seq)
-            print('New score:' + str(new_score))
-            mutation_map[i, j] = (new_score - score) * max(0, score, new_score)
+            mutation_map[i, j] = new_score - score
 
     return mutation_map.T
 
 
-def get_mutation_map(map):
+def generate_mutation_map_graph(seq, mutation_map):
     """
     Given a mutation map, generate an interaction logo
     """
-    pass
+    bases = "ACGT"
+
+    fig, ax = plt.subplots()
+    im, cbar = heatmap(data=mutation_map, row_labels=bases, col_labels=seq, ax=ax,
+                       cmap="RdBu_r", cbarlabel="Change in score from Mutation")
+    texts = annotate_heatmap(im, valfmt="{x:.1f}")
+
+    fig.tight_layout()
+    plt.savefig('figure.png')
+
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_xticklabels(col_labels)
+
+    ax.set_yticks(np.arange(data.shape[0]))
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=0, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1] + 1) - .5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0] + 1) - .5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max()) / 2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
+
+seq = "GCGCCTCACC"
+seq1 = "GCGCCGCCGGAACCCAGTTTCTCTCGAGGTCTTCGCCCCGAGCGAGGCCCGGCGGGGACTCCCGGAGGCCTCAAGGGCCCTCAGCTAGGCGCCACCACCTC"
+
+model = read_file("./static/model.pickle").to(device)
+mutation_map = get_mutation_map(seq1, model)
+generate_mutation_map_graph(seq1, mutation_map)
